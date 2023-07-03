c0d5215b6fbc0abd19cc0410f7f046966b0040bf
Exam Test Account <exam0@BRIDGE.ENTERPRISE>
2023-07-03 10:30:33 +0100

    Added components

--- Employer Project/src/App.tsx
+++ Employer Project/src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainMenu from './components/MainMenu'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      
+      <MainMenu />
     </div>
   )
 }
