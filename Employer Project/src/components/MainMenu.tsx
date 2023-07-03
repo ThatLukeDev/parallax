fee7bc139fb2f210388a5c35ef0827bfd8fbe1ce
Luke Hardaker <Luke@BRIDGE.ENTERPRISE>
2023-07-03 12:55:49 +0100

    merge

--- Employer Project/src/components/MainMenu.tsx
+++ Employer Project/src/components/MainMenu.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const MainMenu = () => {
   return (
-    <div>
-        
+    <div className='mainOuter'>
+      <h1 className='titleFont largeText primaryColour'>CARD FLIPPER</h1>
+      <div>
+        <input type="text" className='secondaryColour mediumText' placeholder='username...' />
+        <button className='removeBorder secondaryColour mediumText'>PLAY</button>
+      </div>
     </div>
   )
 }
