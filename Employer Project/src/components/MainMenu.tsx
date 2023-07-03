8288ce61975f43f802f5109e11f0ff5a7c4c8d86
Luke Hardaker <Luke@BRIDGE.ENTERPRISE>
2023-07-03 13:02:24 +0100

    merge

--- Employer Project/src/components/MainMenu.tsx
+++ Employer Project/src/components/MainMenu.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const MainMenu = () => {
+  let getCookie = (name : string) => {
+    return document.cookie.split(name + "=")[1].split(";")[0]
+  }
+  let setCookie = (name : string, content : string) => {
+    document.cookie = name + "=" + content;
+  }
+  let play = () => {
+    setCookie("currentPlayer", document.getElementById("playernameBox")!.value)
+  }
   return (
     <div className='mainOuter'>
       <h1 className='titleFont largeText primaryColour'>CARD FLIPPER</h1>
       <div>
-        <input type="text" className='secondaryColour mediumText' placeholder='username...' />
-        <button className='removeBorder secondaryColour mediumText'>PLAY</button>
+        <input type="text" className='secondaryColour mediumText' id="playernameBox" placeholder='username...' />
+        <button className='removeBorder secondaryColour mediumText' onClick={() => play()}>PLAY</button>
       </div>
     </div>
   )
