fcfab509b06e9fb0ae52e780b8e7418b57fab8ff
Luke Hardaker <Luke@BRIDGE.ENTERPRISE>
2023-07-04 10:02:21 +0100

    made card adding automated

--- Employer Project/src/components/MainMenu.tsx
+++ Employer Project/src/components/MainMenu.tsx
@@ -2,22 +2,6 @@ import { useEffect } from 'react'
 import { getCookie, setCookie, getPlayerCookie, setPlayerCookie, updateCurrentPlayerCookie } from '../App';
 
 const MainMenu = () => {
-  /*let getCookie = (name : string) => {
-    return document.cookie.split(name + "=")[1].split(";")[0];
-  }
-  let setCookie = (name : string, content : string) => {
-    document.cookie = name + "=" + content;
-  }
-  let setPlayerCookie = (username : string, difficulty : number, highscore : number) => {
-    setCookie(username, `${username}, ${difficulty.toString()}, ${highscore.toString()}`);
-  }
-  let getPlayerCookie = (username : string) => {
-    let cook = getCookie(username).split(", ");
-    return [cook[0], cook[1], cook[2]];
-  }
-  let updateCurrentPlayerCookie = () => {
-    setPlayerCookie(getCookie("currentPlayer"), getCookie("amountOfCards"), getCookie("highscore"))
-  }*/
   let play = () => {
     setCookie("currentPlayer", document.getElementById("playernameBox")!.value);
     setCookie("highscore", "0");
@@ -34,9 +18,14 @@ const MainMenu = () => {
     })
   }
   useEffect(() => {
+    document.getElementById("cardSelections")!.innerHTML = ""
+    for (let i = 4; i < 21; i += 4) {
+      document.getElementById("cardSelections")!.innerHTML += `<button class='removeBorder secondaryColour smallText squareBtn' onClick="document.cookie = 'amountOfCards=${i}'; document.getElementById('cardSelections')?.childNodes.forEach((x) => { if (x.innerText == '${i}') { x.classList.remove('primaryButtonColour'); } else { x.classList.add('primaryButtonColour'); }})">${i}</button>`
+    }
     chooseAmount(4);
   }, [])
   
+  //      <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(4)}>4</button>
   return (
     <div className='mainOuter'>
       <h1 className='titleFont largeText primaryColour'>CARD FLIPPER</h1>
@@ -45,11 +34,7 @@ const MainMenu = () => {
         <button className='removeBorder secondaryColour mediumText' onClick={() => play()}>PLAY</button>
       </div>
       <div id="cardSelections">
-        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(4)}>4</button>
-        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(8)}>8</button>
-        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(12)}>12</button>
-        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(16)}>16</button>
-        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(20)}>20</button>
+        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(1)}>4</button>
       </div>
     </div>
   )
