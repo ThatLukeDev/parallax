ebf583b144f2f7ad95488649a8cf18fb8ea63b17
ThatLukeDev <luke@l2j.co.uk>
2023-07-04 22:01:20 +0100

    fixed amount of cards inconsistant on game.tsx

--- Employer Project/src/components/Game.tsx
+++ Employer Project/src/components/Game.tsx
@@ -3,6 +3,7 @@ import Card from './Card'
 import { getCookie, setCookie, getPlayerCookie, setPlayerCookie, updateCurrentPlayerCookie } from '../App';
 
 const Game = ({gameOver}) => {
+  const distinctCards = 10;
   let cardsOnTable = []
   let usedCards = []
   const [cards, setCards] = useState<number[]>()
@@ -13,6 +14,7 @@ const Game = ({gameOver}) => {
   const [cardFlippedNo, setCardFlippedNo] = useState([])
   let useCardsAmount = 10;
   useEffect(() => {
+    /*
     let count = 0
     useCardsAmount = parseInt(getCookie("amountOfCards")) + 2
     for (let i = 0; i < 9; i++) {
@@ -32,9 +34,26 @@ const Game = ({gameOver}) => {
         count += 2
       }
     }
-    setCards(cardsOnTable)
-    selectedCard = -1
-    selectedKey = -1
+    */
+    let amountOfCards = parseInt(getCookie("amountOfCards")!);
+    let tableCards = Array(amountOfCards).fill(-1);
+    for (let i = 0; i < amountOfCards; i += 2) {
+      let randCard = 0;
+      do {
+        randCard = Math.floor(Math.random() * distinctCards);
+      } while (usedCards.includes(randCard))
+      for (let j = 0; j < 2; j++) {
+        let randIndex = 0;
+        do {
+          randIndex = Math.floor(Math.random() * amountOfCards);
+        } while (tableCards[randIndex] != -1)
+        tableCards[randIndex] = randCard;
+      }
+    }
+    cardsOnTable = tableCards;
+    setCards(cardsOnTable);
+    selectedCard = -1;
+    selectedKey = -1;
 
   }, [])
   let arr = []
