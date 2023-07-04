298b8f6e88b452af086da4d61e45efdaa3d9a281
ThatLukeDev <luke@l2j.co.uk>
2023-07-04 21:21:37 +0100

    fixed stuff

--- Employer Project/src/components/ScoreComponent.tsx
+++ Employer Project/src/components/ScoreComponent.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const ScoreComponent = ({username, cards, score}) => {
   const removeSelf = () => {
+    document.getElementById(`scoreboardBody_${username}`)!.outerHTML = "";
+    document.cookie = `PLAYER_${username}=; Max-Age=-99999999;`
   }
   return (
-    <div className='scoreRow'>
+    <div className='scoreRow' id={`scoreboardBody_${username}`}>
       <button className='squareBtn2 removeBorder' onClick={() => removeSelf()}>X</button>
       <h1>{username}</h1>
       <h1 className='rightElementTwo'>{cards}</h1>
