0e38941a9196eaef2e0829fa3a3f9811f3f41387
Jakub Szamuk <lavaproofJakub1@gmail.com>
2023-07-04 08:45:02 +0100

    Card generation working (finally)

--- Employer Project/src/components/Game.tsx
+++ Employer Project/src/components/Game.tsx
@@ -1,31 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 const Game = () => {
-  const findElement = (arrayTwo, searchFor) => {
-    for (let j = 0; j < arrayTwo.Length; j++) {
-      console.log(arrayTwo.length)
-      if (arrayTwo[j] == searchFor) {
-        return true
+  let cardsOnTable = [];
+  let usedCards = [];
+  const [cards, setCards] = useState<number[]>()
+  useEffect(() => {
+    let count = 0;
+    for (let i = 0; i < 6; i++) {
+      let randIndex = 0
+      let randIndexTwo = 0
+      do {
+        randIndex = Math.floor(Math.random() * (10 - 1) ) + 1
+      }  while (usedCards.includes(randIndex))
+      do {
+        randIndexTwo = Math.floor(Math.random() * (10 - 1) ) + 1
+      } while (usedCards.includes(randIndexTwo) && randIndex != randIndexTwo)
+      if (!usedCards.includes(randIndex) && !usedCards.includes(randIndexTwo) && randIndex != 0 && randIndexTwo != 0 && randIndex != randIndexTwo) {
+        cardsOnTable[randIndex] = i;
+        cardsOnTable[randIndexTwo] = i;
+        usedCards[count] = randIndex;
+        usedCards[count + 1] = randIndexTwo;
+        count += 2
       }
     }
-    return false
-  }
-  let cardsOnTable = [];
-  let usedCards:number[] = []
-  let usedIndexes:number[] = []
-  useEffect(() => {
-    for (let i = 0; i < 5; i++) {
-      let randContent = 0
-      do {
-        randContent = Math.floor(Math.random() * (5 - 1) ) + 1
-      } while (findElement(usedCards, randContent))
-      usedCards.push(randContent)
-      let randIndex = Math.floor(Math.random() * (10 - 1) ) + 1
-      let randIndexTwo = Math.floor(Math.random() * (10 - 1) ) + 1
-      cardsOnTable[randIndex] = randContent;
-      cardsOnTable[randIndexTwo] = randContent;
-    }
+    console.log(usedCards)
+    setCards(cardsOnTable)
+    console.log(cardsOnTable)
   }, [])
   const cardClicked = (cardKey:number) => {
     console.log(cardKey)
@@ -34,7 +35,7 @@ const Game = () => {
 
   return (
     <div className='cardHolder'>
-        {cardsOnTable.map((card, key) => <Card cardClicked={cardClicked} contents={card} number={key}/>)}
+        {cards ? cards.map((card, key) => <Card cardClicked={cardClicked} contents={card} number={key}/>) : null}
     </div>
   )
 }
