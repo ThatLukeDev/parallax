b8d0146e59d5c0ae6c2e3f3d3fc142e9bac835a3
Luke Hardaker <Luke@BRIDGE.ENTERPRISE>
2023-07-04 10:31:49 +0100

    scoreboard backbones

--- Employer Project/src/components/MainMenu.tsx
+++ Employer Project/src/components/MainMenu.tsx
@@ -2,6 +2,9 @@ import { useEffect } from 'react'
 import { getCookie, setCookie, getPlayerCookie, setPlayerCookie, updateCurrentPlayerCookie } from '../App';
 
 const MainMenu = () => {
+  let enterToScoreboard = (username : string, amountOfCards : number, highscore : number) => {
+    let scoreboardEntry = `<tr><td>${username}</td><td>${amountOfCards.toString()}</td><td>${highscore.toString()}</td></tr>`
+  }
   let play = () => {
     setCookie("currentPlayer", document.getElementById("playernameBox")!.value);
     setCookie("highscore", "0");
@@ -25,16 +28,26 @@ const MainMenu = () => {
     chooseAmount(4);
   }, [])
   
-  //      <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(4)}>4</button>
   return (
-    <div className='mainOuter'>
-      <h1 className='titleFont largeText primaryColour'>CARD FLIPPER</h1>
-      <div>
-        <input type="text" className='secondaryColour mediumText' id="playernameBox" placeholder='Username...' />
-        <button className='removeBorder secondaryColour mediumText' onClick={() => play()}>PLAY</button>
+    <div>
+      <div className='mainOuter'>
+        <h1 className='titleFont largeText primaryColour'>CARD FLIPPER</h1>
+        <div>
+          <input type="text" className='secondaryColour mediumText' id="playernameBox" placeholder='Username...' />
+          <button className='removeBorder secondaryColour mediumText' onClick={() => play()}>PLAY</button>
+        </div>
+        <div id="cardSelections">
+          <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(1)}>4</button>
+        </div>
       </div>
-      <div id="cardSelections">
-        <button className='removeBorder secondaryColour smallText squareBtn' onClick={() => chooseAmount(1)}>4</button>
+      <div className='primaryColour' id='scoreboard'>
+        <table>
+          <tr>
+            <th>USERNAME</th>
+            <th>CARDS</th>
+            <th>SCORE</th>
+          </tr>
+        </table>
       </div>
     </div>
   )
